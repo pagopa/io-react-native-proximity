b760a062526670169142b2722243cc0bd9e8bb63
grausof <grausof@gmail.com>
2023-11-20 17:39:01 +0100

    Add comment

--- src/proximity-manager/session.ts
+++ src/proximity-manager/session.ts
@@ -2,16 +2,25 @@ import { rawToJwkEc, type JWK } from '../utils/jwk';
 
 import * as crypto from 'crypto';
 
+/*
+ * Represents a session for exchanging information between an mDoc and an mDoc reader
+ */
 const Session = () => {
-  let isStarted = false;
-  let sessionPublicJwk: JWK;
+  let isStarted = false; // Indicates if the session has been started or not
+  let sessionPublicJwk: JWK; // The session's public key in JWK format
+  let sessionKeys: crypto.ECDH;
 
+  /*
+   * Starts the session. Returns an exception if the session has already been started
+   */
   const start = () => {
     return new Promise<string>(async (resolve, reject) => {
       if (!isStarted) {
-        let sessionKeys = crypto.createECDH('secp256r1');
+        // Generates an ephemeral key pair of elliptic type (secp256)
+        sessionKeys = crypto.createECDH('secp256r1');
         sessionKeys.generateKeys();
 
+        // Converts the public key into JWK format
         sessionPublicJwk = rawToJwkEc(sessionKeys.getPublicKey('hex'));
         isStarted = true;
         resolve('START');
@@ -21,6 +30,9 @@ const Session = () => {
     });
   };
 
+  /*
+   * Returns the session public key in JWK format
+   */
   const getSessionPublicKey = () => {
     return new Promise<JWK>((resolve, reject) => {
       if (isStarted) {
@@ -31,6 +43,9 @@ const Session = () => {
     });
   };
 
+  /*
+   * Closes the session. Returns an exception if the session is not started
+   */
   const close = () => {
     return new Promise<string>((resolve, reject) => {
       if (isStarted) {
