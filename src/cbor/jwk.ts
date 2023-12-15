801dbaca2ab3aaf30a7a2b5083dba87a8f533dd0
grausof <grausof@gmail.com>
2023-12-15 17:40:08 +0100

    Remove key parser

--- src/cbor/jwk.ts
+++ src/cbor/jwk.ts
@@ -1,38 +1,37 @@
 import { decode, encode } from '.';
 import type { JWK } from '../utils/jwk';
-import cosekey from 'parse-cosekey';
 
 export const fromJwkToCoseHex = (jwk: JWK) => {
-  const key = cosekey.KeyParser.jwk2cose(jwk);
-  return Buffer.from(encode(key)).toString('hex');
+  console.log('fromJwkToCoseHex', fromJwkToCoseHex);
+  if (jwk.kty === 'EC') {
+    const key = new Map();
+    key.set(1, 2); //KID
+    key.set(-1, 1); //Type (1 = EC P-256 / secp256r1)
+    key.set(-2, Buffer.from(jwk.x, 'base64')); // X
+    key.set(-3, Buffer.from(jwk.y, 'base64')); // y
+    return Buffer.from(encode(key)).toString('hex');
+  } else {
+    throw new Error('Not implemented');
+  }
 };
 
 export const fromCoseToJwk = (buffer: Buffer): JWK => {
   const keyBuffer = decode(buffer);
-  const parsedJwk = cosekey.KeyParser.cose2jwk(keyBuffer);
-  let finalJwk: JWK;
+  const kid = keyBuffer.get(1);
+  const typ = keyBuffer.get(-1);
+  const x = Buffer.from(keyBuffer.get(-2));
+  const y = Buffer.from(keyBuffer.get(-3));
 
-  // the library does not return a hexadecimal string but a buffer
-  if (parsedJwk.kty && parsedJwk.crv && parsedJwk.x && parsedJwk.y) {
-    finalJwk = {
-      kty: parsedJwk.kty,
-      crv: parsedJwk.crv,
-      x: Buffer.from(parsedJwk.x).toString('hex'),
-      y: Buffer.from(parsedJwk.y).toString('hex'),
+  if (kid === 2 && typ === 1) {
+    let finalJwk: JWK = {
+      kty: 'EC',
+      crv: 'P-256',
+      x: x.toString('hex'),
+      y: y.toString('hex'),
     };
-
-    if (parsedJwk.d) {
-      finalJwk.d = Buffer.from(parsedJwk.d).toString('hex');
-    }
-    if (parsedJwk.n) {
-      finalJwk.n = Buffer.from(parsedJwk.n).toString('hex');
-    }
-    if (parsedJwk.e) {
-      finalJwk.e = Buffer.from(parsedJwk.e).toString('hex');
-    }
-
+    console.log('finalJwk', finalJwk);
     return finalJwk;
   } else {
-    throw new Error('Unable to convert key from COSE to JWK');
+    throw new Error('Not implemented');
   }
 };
