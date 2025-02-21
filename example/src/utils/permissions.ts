dd43a47bcb04175835a132b5df57e8167c26d851
Alessandro Mazzon <alessandro.mazzon@pagopa.it>
2025-02-21 17:54:43 +0100

    fix: android permissions for api level < 31

--- example/src/utils/permissions.ts
+++ example/src/utils/permissions.ts
@@ -1,26 +1,35 @@
 import { Platform } from 'react-native';
 import {
   checkMultiple,
+  type Permission,
   PERMISSIONS,
   requestMultiple,
   RESULTS,
 } from 'react-native-permissions';
 
 export const requestBlePermissions = async (): Promise<boolean> => {
-  const AndroidPermissions = [
-    PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
-    PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
-    PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
-    PERMISSIONS.ANDROID.BLUETOOTH_ADVERTISE,
-  ];
+  let permissionsToCheck: Permission[];
 
-  const iOSPermissions = [
-    PERMISSIONS.IOS.BLUETOOTH,
-    PERMISSIONS.IOS.LOCATION_ALWAYS,
-  ];
-
-  const permissionsToCheck =
-    Platform.OS === 'android' ? AndroidPermissions : iOSPermissions;
+  if (Platform.OS === 'android') {
+    if (Platform.Version >= 31) {
+      // Android 12 and above: Request new Bluetooth permissions along with location.
+      permissionsToCheck = [
+        PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
+        PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+        PERMISSIONS.ANDROID.BLUETOOTH_ADVERTISE,
+      ];
+    } else {
+      // Android 9 to Android 11: Only location permission is required for BLE.
+      permissionsToCheck = [PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION];
+    }
+  } else {
+    // iOS permissions remain unchanged.
+    permissionsToCheck = [
+      PERMISSIONS.IOS.BLUETOOTH,
+      PERMISSIONS.IOS.LOCATION_ALWAYS,
+    ];
+  }
 
   try {
     // Check current permission status
