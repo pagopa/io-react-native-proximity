1c342334b7a20f1d0ff3463057a671416a4a5139
LazyAfternoons <lazyafternoons@outlook.it>
2025-04-10 11:01:07 +0200

    docs: update documentation

--- src/index.tsx
+++ src/index.tsx
@@ -24,6 +24,7 @@ export type QrEngagementEventPayloads = {
   onConnecting: undefined;
   onDeviceRetrievalHelperReady: undefined;
   onCommunicationError: { error: string } | undefined;
+  // The message payload is a JSON string that must be parsed into a `VerifierRequest` structure via `parseVerifierRequest`.
   onNewDeviceRequest: { message: string } | undefined;
   onDeviceDisconnected: undefined;
 };
@@ -65,9 +66,7 @@ export function initializeQrEngagement(
 }
 
 /**
- * Gets the QR code string
- * @ios This method is responsible for initializing the connection and retrieving the QR code string
- * @android Returns the QR code string
+ * Gets the QR code string this method is responsible for initializing the connection and retrieving the QR code string
  */
 export function getQrCodeString(): Promise<string> {
   return IoReactNativeProximity.getQrCodeString();
@@ -85,13 +84,14 @@ export function closeQrEngagement(): Promise<boolean> {
  */
 export function sendErrorResponse(): Promise<boolean> {
   if (Platform.OS === 'ios') {
+    // Currently not implemented for iOS
     return Promise.resolve(true);
   }
   return IoReactNativeProximity.sendErrorResponse();
 }
 
 /**
- * Sends an error response to the verifier app when the requested document is not found
+ * Sends an error response to the verifier app when the requested document is not found.
  */
 export function sendErrorResponseNoData(): Promise<boolean> {
   return IoReactNativeProximity.sendErrorResponseNoData();
@@ -99,11 +99,20 @@ export function sendErrorResponseNoData(): Promise<boolean> {
 
 /**
  * Generates a response that will be sent to the verifier app containing the requested data
- * @param issuerSignedContent - Base64 encoded string that represents the CBOR document
- * @param alias - The key alias to use for signing the response
- * @param docType - The document type of issuerSignedContent
- * @param fieldRequestedAndAccepted - JSON object containing the accepted fields
- * @param alias - The key alias to use for signing the response
+ * @param documents - An array of `Document` which contains the requested data received from the `onNewDeviceRequest` event
+ * @param acceptedFields - The accepted fields which will be presented to the verifier app. This is the same as the `request` field in the `VerifierRequest` object.
+ * The outermost key represents the credential doctype. The inner dictionary contains namespaces, and for each namespace, there is another dictionary mapping requested claims to a boolean value, which indicates whether the user is willing to present the corresponding claim. Example:
+ * `{
+ *    "org.iso.18013.5.1.mDL": {
+ *      "org.iso.18013.5.1": {
+ *        "hair_colour": true,
+ *        "given_name_national_character": true,
+ *        "family_name_national_character": true,
+ *        "given_name": true,
+ *     }
+ *    }
+ *  }`
+ * @returns A base64 encoded response to be sent to the verifier app via `sendResponse`
  */
 export function generateResponse(
   documents: Array<Document>,
@@ -112,10 +121,21 @@ export function generateResponse(
   return IoReactNativeProximity.generateResponse(documents, acceptedFields);
 }
 
+/**
+ * Sends the response generated through the `generateResponse` method to the verifier app.
+ * Currently there's not evidence of the verifier app responding to this request, thus we don't handle the response.
+ * @param response - The base64 encoded response to be sent to the verifier app.
+ */
 export function sendResponse(response: string): Promise<boolean> {
   return IoReactNativeProximity.sendResponse(response);
 }
 
+/**
+ * Adds a listener for a `QrEngagementEvents` event which will be emitted by the native module.
+ * The callback will be called with the event payload when the event is emitted.
+ * @param event - The event to listen for. The available events are defined in the `QrEngagementEvents` type.
+ * @param callback - The callback to be called when the event is emitted. The callback will receive the event payload as an argument.
+ */
 export function addListener<E extends QrEngagementEvents>(
   event: E,
   callback: (data: QrEngagementEventPayloads[E]) => void
@@ -123,6 +143,10 @@ export function addListener<E extends QrEngagementEvents>(
   eventEmitter.addListener(event, callback);
 }
 
+/**
+ * Removes a listener for a `QrEngagementEvents` event.
+ * @param event - The event to remove the listener for. The available events are defined in the `QrEngagementEvents` type.
+ */
 export function removeListeners(event: QrEngagementEvents) {
   eventEmitter.removeAllListeners(event);
 }
--- src/schema.ts
+++ src/schema.ts
@@ -5,8 +5,32 @@ const VerifierRequest = z.object({
   request: z.record(z.string(), z.record(z.record(z.string(), z.boolean()))),
 });
 
+/**
+ * VerifierRequest type returned by the `onNewDeviceRequest` event in `QrEngagementEvents`. The outermost key represents the credential doctype.
+ * - isAuthenticated: A boolean value indicating whether the verifier app is verified or not
+ * - request: The inner dictionary contains namespaces, and for each namespace, there is another dictionary mapping requested claims to a boolean value,
+ * which indicates whether the verfier app wants to retain the claim or not. Example:
+ *  `{
+ *    "org.iso.18013.5.1.mDL": {
+ *      "org.iso.18013.5.1": {
+ *        "hair_colour": true,
+ *        "given_name_national_character": true,
+ *        "family_name_national_character": true,
+ *        "given_name": true,
+ *      }
+ *    }
+ *  }`
+ * The request type can be also be used as input for the `generateResponse` method. The structure is the same, however the boolean value for each claim
+ * indicates the willing to present the claim.
+ */
 export type VerifierRequest = z.infer<typeof VerifierRequest>;
 
+/**
+ * Parses the input to a VerifierRequest object.
+ * This function is used to parse the request received from the verifier app.
+ * @param input - The input to be parsed
+ * @returns The parsed VerifierRequest object
+ */
 export const parseVerifierRequest = (input: unknown): VerifierRequest => {
   return VerifierRequest.parse(input);
 };
